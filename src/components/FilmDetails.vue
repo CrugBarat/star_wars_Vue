ff00e5611fae02862d1fb51f0994b10f62394540
Craig Barratt <craigtbarratt@gmail.com>
2020-04-27 22:33:47 +0100

    populates film details with film data

--- src/components/FilmDetails.vue
+++ src/components/FilmDetails.vue
@@ -1,9 +1,18 @@
 <template lang="html">
-
+  <div class="">
+    <p>{{film.title}}</p>
+    <p>Episode: {{film.episode_id}}</p>
+    <p>Director: {{film.director}}</p>
+    <p>Producer(s): {{film.producer}}</p>
+    <p>Release Date: {{film.release_date}}</p>
+    <p>Characters:</p>
+  </div>
 </template>
 
 <script>
 export default {
+  name: 'film-details',
+  props: ['film']
 }
 </script>
 
--- src/components/FilmList.vue
+++ src/components/FilmList.vue
@@ -1,14 +1,22 @@
 <template lang="html">
   <div>
     <p>Film list:</p>
-    <p v-for="film in films">{{film.title}}</p>
+    <div v-for="film in films">
+      <p>{{film.title}}</p>
+      <film-details :film="film"></film-details>
+    </div>
   </div>
 </template>
 
 <script>
+import FilmDetails from '@/components/FilmDetails.vue'
+
 export default {
   name: 'film-list',
-  props: ['films']
+  props: ['films'],
+  components: {
+    'film-details': FilmDetails
+  }
 }
 </script>
 
