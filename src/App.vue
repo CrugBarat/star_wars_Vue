7506029872d85954df395c6ced511f8b31489acc
Craig Barratt <craigtbarratt@gmail.com>
2020-04-28 19:44:50 +0100

    clears header for opening crawl

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,47 @@
 <template>
   <div>
-    <router-link class="link" :to="{name: 'home'}">
-      <div class="title">
-        <p>Star</p>
-      </div>
-      <div class="title">
-        <p>Wars</p>
-      </div>
-    </router-link>
-      <router-view></router-view>
-    </div>
-  </template>
+    <div v-if="['opening_crawl'].indexOf($route.name)">
+      <router-link class="link" :to="{name: 'home'}">
+        <div class="title">
+          <p>STAR WARS</p>
+        </div>
+      </router-link>
+    <router-view></router-view>
+  </div>
+</div>
+</template>
 
 <script>
-  export default {
-    name: 'app'
-  }
+    export default {
+      name: 'app'
+    }
 </script>
 
 
 <style lang="css" scoped>
-  .link {
-    text-decoration: none;
-    cursor: url('./assets/lightsaber.png'), auto;
-  }
+    .link {
+      text-decoration: none;
+      cursor: url('./assets/lightsaber.png'), auto;
+    }
 
-  .title {
-    font-family: 'Jedi';
-    font-size: 10vw;
-    text-align: center;
-    color: black;
-    opacity: 80%;
-    text-shadow: -1px -1px 0 #f9D71C, 1px -1px 0 #f9D71C, -1px 1px 0 #f9D71C, 1px 1px 0 #f9D71C;
-  }
+    .title {
+      font-family: 'Jedi';
+      font-size: 10vw;
+      text-align: center;
+      color: black;
+      opacity: 80%;
+      text-shadow: -1px -1px 0 #f9D71C, 1px -1px 0 #f9D71C, -1px 1px 0 #f9D71C, 1px 1px 0 #f9D71C;
+    }
 
-  p {
-    padding: 0;
-    margin: 0;
-  }
+    p {
+      padding: 0;
+      margin: 0;
+    }
 </style>
 
 <style>
   body {
-    background-image: url('./assets/background.jpg');
-    background-repeat: no-repeat;
-    background-size: cover;
-    background-attachment: fixed;
+    background-color: black;
     font-family: 'Franklin';
     color: #f9D71C;
     cursor: url('./assets/lightsaber.png'), auto;
